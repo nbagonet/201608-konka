021f720ff5bef06614480174e6f6fd3948591d8f
王硕 <101840192@qq.com>
2016-08-10 15:47:21 +0800

    场景1

---
+++ src/components/section1/index.vue
@@ -0,0 +1,44 @@
+<template lang="jade">
+  #section1
+    //- 过场闪光
+    #section1-light
+
+    //- 背景
+    #section1-bg
+
+    //- 电视机
+    - for(var i=0;i<3;i++)
+      div(id='section1-tv#{i}')
+        .screen
+        .tv
+        .land
+
+    //- 文字
+    #section1-txt0
+    #section1-txt1
+</template>
+
+<style lang="less">
+@import './index.less';
+</style>
+
+<script>
+import { init } from '../../vuex/actions/section1'
+
+export default {
+  name: 'section1',
+  vuex: {
+    getters: {},
+    actions: {
+      init
+    }
+  },
+  components: {},
+  data: function () {
+    return {}
+  },
+  ready: function () {
+    this.init()
+  }
+}
+</script>
