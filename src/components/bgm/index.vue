1771b83155873212bf9d07d70cae72b512a790dd
王硕 <101840192@qq.com>
2016-08-10 18:04:46 +0800

    背景音乐模块

---
+++ src/components/bgm/index.vue
@@ -0,0 +1,43 @@
+<template lang="jade">
+  #bgm
+    audio#bgm-audio(src='../../../static/bgm.mp3' loop)
+    .btn.play(v-show='isPlaying' @click='pause')
+    .btn.pause(v-else @click='play')
+</template>
+
+<style lang="less" scoped>
+@import './index.less';
+</style>
+
+<script>
+export default {
+  name: 'bgm',
+  vuex: {
+    getters: {},
+    actions: {}
+  },
+  components: {},
+  data: function () {
+    return {
+      isPlaying: false
+    }
+  },
+  methods: {
+    play: function () {
+      window.$('#bgm-audio')[0].play()
+      this.$set('isPlaying', true)
+    },
+    pause: function () {
+      window.$('#bgm-audio')[0].pause()
+      this.$set('isPlaying', false)
+    }
+  },
+  ready: function () {
+    const _this = this
+    document.addEventListener('WeixinJSBridgeReady', function () {
+      _this.play()
+    })
+    _this.play()
+  }
+}
+</script>
